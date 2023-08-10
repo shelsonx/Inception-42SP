d435dcc14e1c6893ea09fe78080f0df7d6449cef
shelsonx <shelsonx@gmail.com>
2023-08-10 00:30:37 -0300

    create a new user in wordpress

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -2,8 +2,8 @@ if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
 
 	wp core config \
 		--dbname=$WP_DATABASE \
-		--dbuser=$WP_USER \
-		--dbpass=$WP_PASSWORD \
+		--dbuser=$WP_MDB_USER \
+		--dbpass=$WP_MDB_PASSWORD \
 		--dbhost=$WP_HOST \
 		--dbprefix='wp_' \
 		--dbcharset="utf8" \
@@ -16,4 +16,5 @@ if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
         --admin_password=$WP_ADMIN_PASSWORD \
         --admin_email=$WP_ADMIN_EMAIL \
         --skip-email --allow-root
+	wp user create $WP_USER $WP_EMAIL --user_pass=$WP_PASSWORD --role=subscriber --porcelain --allow-root
 fi
